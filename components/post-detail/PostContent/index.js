6301062258c617200fc302a471514cc3c61d00a8
AntonKolomiiets <anton.kolomiiets@logz.io>
2021-11-30 22:03:55 +0200

    add new postContent and fetch data

--- components/post-detail/PostContent/index.js
+++ components/post-detail/PostContent/index.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from "react-markdown";
 import PostHeader from "../PostHeader";
 import classes from "./PostContent.module.css";
 
@@ -13,9 +14,9 @@ function PostContent() {
 	const imagePath = `/images/post/${DUMMY_POST.slug}/ ${DUMMY_POST.image}`;
 
 	return (
-		<article>
+		<article className={classes.content}>
 			<PostHeader title={DUMMY_POST.title} image={imagePath} />
-			content
+			<ReactMarkdown> {DUMMY_POST.content}</ReactMarkdown>
 		</article>
 	);
 }
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,12 @@
+---
+title: Getting started with NextJs
+date: "2022-10-15"
+author: Anton Kolomiiets
+image: getting-started-nextjs.png
+excerpt: description short
+isFeatured: true
+---
+
+# This is a title
+
+this is some regular text a [link](https://logz.io)
